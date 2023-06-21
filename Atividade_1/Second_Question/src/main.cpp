738421f0439d091f77939233521bf74961c3eeb4
Renzo Zukeram <115081124+renzozuk@users.noreply.github.com>
2023-06-20 22:44:29 -0300

    Update main.cpp
    Some commentaries were added to the main.cpp file.

--- Atividade_1/Second_Question/src/main.cpp
+++ Atividade_1/Second_Question/src/main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main(){
     zoo z;
-    //z.addAnimal nome especie idade 
-    z.addAnimal("Luan", "passaro", 2);
+    
+    //add animals to zoo (name, specie, age)
+    z.addAnimal("Luan", "pássaro", 2);
     z.addAnimal("Bartolomeu", "jaguatirica", 3);
     z.addAnimal("Loki", "lobo", 3);
-    z.addAnimal("Simba", "leao", 4);
-    z.addAnimal("Nala", "leao", 4);
+    z.addAnimal("Simba", "leão", 4);
+    z.addAnimal("Nala", "leão", 4);
     z.addAnimal("Slytherio", "cobra", 22);
-    z.addAnimal("Mufasa", "leao", 7);
-    z.addAnimal("Scar", "leao", 7);
-    z.addAnimal("Lara", "passaro", 3);
+    z.addAnimal("Mufasa", "leão", 7);
+    z.addAnimal("Scar", "leão", 7);
+    z.addAnimal("Lara", "pássaro", 3);
     z.addAnimal("Frederico", "jaguatirica", 4);
     z.addAnimal("Katy", "jaguatirica", 1);
     z.addAnimal("Princess", "tartaruga", 17);
@@ -22,6 +23,12 @@ int main(){
     z.addAnimal("Nafu", "macaco", 4);
     z.addAnimal("Genisvaldo", "tartaruga", 56);
     z.addAnimal("Suassuna", "cobra", 8);
+    
+    //show the options:
+    //- print all the animals of the list
+    //- print the animals by specie
+    //- remove the animals of the list by age (the animals that's the age chosen or above will be removed)
     z.gcmd();
+    
     return 0;
 }
